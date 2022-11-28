abb77d9fe99cf8ac0948b161ed7a300e74fcbc13
LiquidArtDevelopers <aranaz@gmail.com>
2022-11-28 18:06:40 +0100

    mejoras

--- enviar.php
+++ enviar.php
@@ -1,12 +1,16 @@
 <?php
 
     $destino = "aranaz@gmail.com";
+    
     $nombre = $_POST["Nombre"];
-    $correo = $_POST["Telefono"];
-    $telefono = $_POST["Correo"];
+    $correo = $_POST["Correo"];
+    $telefono = $_POST["Telefono"];
     $mensaje = $_POST["Mensaje"];
+
     $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nMensaje: " . $mensaje;
 
-    mail($destino,"Consulta de la Web",$contenido);
+    mail($destino,"Consulta de la Web",$contenido); /* correo que recibo yo */
+    mail($correo,"Hemos recibido tu consulta",$contenido); /* correo que recibe el cliente */
+    
     header("location:gracias.html");
 ?>
